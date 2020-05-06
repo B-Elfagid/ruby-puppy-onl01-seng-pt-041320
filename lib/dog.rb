7dade9111f1688c88796e2fdc14192cd3bd8fd1c
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-06 13:39:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,31 @@
-class Dog 
+class Dog
   @@all = []
-  
-  
+
   def initialize(name)
-    @name = name 
-    self.save 
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-   
-   def self.print_all
+    @name = name
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all = [] 
+  end
+
+  def self.print_all
     @@all.each do |dog|
       puts dog.name
     end
-    
-    def save 
-      @@all << self 
-    end 
-    
-    def self.clear_all
-      @@all << []
-    end 
+end
+
+  def save
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+
 end
